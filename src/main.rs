b0d88951ecb68a3b52ea1823667e583a9184e616
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-04-11 15:11:20 +0700

    add tcp server listener
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+const ADDR: &str = "127.0.0.1:8080";
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(ADDR).unwrap();
+    listener.incoming().for_each(|i| {
+        let stream = i.unwrap();
+        println!("Connection Enstablished!");
+        handle_connection(stream);
+    })
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buffer: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
+    let request: Vec<String> = buffer
+        .lines()
+        .map(Result::unwrap)
+        .take_while(|p| !p.is_empty())
+        .collect();
+    request.iter().for_each(|i| println!("{}", i));
+    println!("");
 }
