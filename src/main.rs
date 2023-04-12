ec315cc167523c55369721f0b4b28c31354667e2
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-04-12 08:56:30 +0700

    multithread server
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
+mod threadpool;
+
 use std::{
     fs::read_to_string,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use threadpool::ThreadPool;
 
 const ADDR: &str = "127.0.0.1:8080";
 
 fn main() {
     let listener = TcpListener::bind(ADDR).unwrap();
+    let pool = ThreadPool::new(2);
+
     listener.incoming().for_each(|i| {
         let stream = i.unwrap();
         println!("Connection Enstablished!");
-        handle_connection(stream);
+        pool.execute(|| handle_connection(stream));
     })
 }
 
@@ -22,11 +30,18 @@ fn handle_connection(mut stream: TcpStream) {
         .map(Result::unwrap)
         .take_while(|p| !p.is_empty())
         .collect();
+
     request.iter().for_each(|i| println!("{}", i));
 
-    let (status, file) = match request.get(0).unwrap() == "GET / HTTP/1.1" {
-        true => ("HTTP/1.1 200 OK", "serve/index.html"),
-        false => ("HTTP/1.1 404 NOT FOUND", "serve/404.html"),
+    let request_line = request.get(0).unwrap();
+
+    let (status, file) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "serve/index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "serve/index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "serve/404.html"),
     };
 
     let content = read_to_string(file).unwrap();
